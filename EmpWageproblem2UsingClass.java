572773fe1aae08696d7a2d327f30913386f59e01
seema thorat <seemagade97@gmail.com>
2021-02-24 14:28:28 +0530

    Multiple Company is  case done

--- EmpWageproblem2UsingClass.java
+++ EmpWageproblem2UsingClass.java
@@ -1,16 +1,31 @@
 public class EmpWageproblem2UsingClass{
 
-		public static final int isPartTime=1;
-      public static final int isFullTime=2;
-      public static final int empRatePerHr=20;
-		public static final int numOfWorkingDays=2;
-		public static final int maxHrsInMonth=10;	
+	public static final int isPartTime=1;
+   public static final int isFullTime=2;
 
-	public static void main(String[] args){
+	private final String company;
+	private final int empRatePerHour;
+	private final int numOfWorkingDays;
+	private final int maxHoursPerMonth;
+
+	public EmpWageproblem2UsingClass(String company, int empRatePerHour, int numOfWorkingDays, int maxHoursPerMonth)
+	{
+		this.company=company;
+		this.empRatePerHour=empRatePerHour;
+		this.numOfWorkingDays=numOfWorkingDays;
+		this.maxHoursPerMonth=maxHoursPerMonth;
+	}
+	
+public static void main(String[] args) 
+{
+	EmpWageproblem2UsingClass emp=new EmpWageproblem2UsingClass("DMart",20,2,10);
+
+	
 	int empHrs=0;
 	int totalEmpHrs=0;
 	int totalWorkingDays=0;
-	while(totalEmpHrs <= maxHrsInMonth && totalWorkingDays < numOfWorkingDays)
+
+	while(totalEmpHrs <= emp.maxHoursPerMonth && totalWorkingDays < emp.numOfWorkingDays)
 	{
 		totalWorkingDays++;
 		int empCheck=(int) Math.floor(Math.random() * 10) %3;
@@ -29,10 +44,12 @@ public class EmpWageproblem2UsingClass{
 			System.out.println("Days: " + totalWorkingDays + "Emp Hrs: " +empHrs);
 
 	}
-int totalEmpWage=totalEmpHrs * empRatePerHr;
-System.out.println("Total Emp Wage: " +totalEmpWage);
+int totalEmpWage=totalEmpHrs * emp.empRatePerHour;
+System.out.println("Total Emp Wage for Company: " + emp.company + " is : " +totalEmpWage);
+
 }
 }
+ 
 
 
 
