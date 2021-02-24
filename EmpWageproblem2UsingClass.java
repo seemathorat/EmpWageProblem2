df4719c67c67060574b6bb87c64fd62b910646d4
seema thorat <seemagade97@gmail.com>
2021-02-24 14:44:44 +0530

    UseCase-9 done

--- EmpWageproblem2UsingClass.java
+++ EmpWageproblem2UsingClass.java
@@ -16,38 +16,42 @@ public class EmpWageproblem2UsingClass{
 		this.maxHoursPerMonth=maxHoursPerMonth;
 	}
 	
+private int computeWage(){
+	 int empHrs=0;
+   int totalEmpHrs=0;
+   int totalWorkingDays=0;
+
+   while(totalEmpHrs <= maxHoursPerMonth && totalWorkingDays < numOfWorkingDays)
+   {
+      totalWorkingDays++;
+      int empCheck=(int) Math.floor(Math.random() * 10) %3;
+
+      switch (empCheck){
+         case isPartTime:
+                     empHrs=4;
+                  break;
+         case isFullTime:
+                     empHrs=8;
+                  break;
+         default:
+               empHrs=0;
+   }
+         totalEmpHrs +=empHrs;
+         System.out.println("Days: " + totalWorkingDays + "Emp Hrs: " +empHrs);
+
+   }
+return totalEmpHrs * empRatePerHour;
+
+}
+
 public static void main(String[] args) 
 {
 	EmpWageproblem2UsingClass emp=new EmpWageproblem2UsingClass("DMart",20,2,10);
-
-	
-	int empHrs=0;
-	int totalEmpHrs=0;
-	int totalWorkingDays=0;
-
-	while(totalEmpHrs <= emp.maxHoursPerMonth && totalWorkingDays < emp.numOfWorkingDays)
-	{
-		totalWorkingDays++;
-		int empCheck=(int) Math.floor(Math.random() * 10) %3;
-				
-		switch (empCheck){
-			case isPartTime:
-							empHrs=4;
-						break;
-			case isFullTime:
-							empHrs=8;
-						break;
-			default:
-					empHrs=0;
-	}
-			totalEmpHrs +=empHrs;
-			System.out.println("Days: " + totalWorkingDays + "Emp Hrs: " +empHrs);
-
-	}
-int totalEmpWage=totalEmpHrs * emp.empRatePerHour;
-System.out.println("Total Emp Wage for Company: " + emp.company + " is : " +totalEmpWage);
-
+	EmpWageproblem2UsingClass emp1=new EmpWageproblem2UsingClass("Reliance",30,3,10);
+	System.out.println("Total Emp Wage For Company " +emp.company + " is: " + emp.computeWage());
+        System.out.println("Total Emp Wage For Company " +emp1.company + " is: " + emp1.computeWage());
 }
+ 
 }
  
 
