8ad8e817c4c438e83aa82152ef5f59f9c3b98339
seema thorat <seemagade97@gmail.com>
2021-02-24 13:27:53 +0530

    UseCse-4 Done

--- EmpWageproblem2UsingClass.java
+++ EmpWageproblem2UsingClass.java
@@ -1,23 +1,28 @@
 public class EmpWageproblem2UsingClass
-{
-        public static void main(String[] args){
-                int isFullTime=1;
-                int empRatePerHr=20;
-        int empHrs=0;
-        int empWage=0;
-                double empCheck=Math.floor(Math.random() * 10) % 2;
+{ 
 
-                if( empCheck == isFullTime )
-                        empHrs=8;
-                else
-                        empHrs=0;
-                        empWage=empHrs * empRatePerHr;
-                        System.out.println("Employee Wage: "+empWage);
-        }
+	 public static final int isPartTime=1;
+      public static final int isFullTime=2;
+      public static final int empRatePerHr=20;
+
+	public static void main(String[] args){
+	int empHrs=0;
+	int empWage=0;
+		int empCheck=(int) Math.floor(Math.random() * 10) % 3;
+		switch (empCheck){
+			case isPartTime:
+							empHrs=4;
+						break;
+			case isFullTime:
+							empHrs=8;
+						break;
+			default:
+					empHrs=0;
+	}
+			empWage=empHrs * empRatePerHr;
+			System.out.println("Employee Wage: "+empWage);
+	}
 }
-
-
-
 
 
 
