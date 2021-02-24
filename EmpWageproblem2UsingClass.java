8602878cd37efe11892b64c318384d3fb0322b77
seema thorat <seemagade97@gmail.com>
2021-02-24 12:40:37 +0530

    employee present or abscent done

--- EmpWageproblem2UsingClass.java
+++ EmpWageproblem2UsingClass.java
@@ -4,8 +4,21 @@ public class EmpWageproblem2UsingClass
       public static void main(String args[])
 {
 
-        System.out.println("Welcome Employee");
+        int is_full_time=1;
+     double empcheck=Math.floor(Math.random() * 10) % 2;
 
+
+   if(empcheck==is_full_time)
+
+{
+   System.out.println("Employee is Present");
+
+  }
+   else
+
+{   
+    System.out.println("Employee is abscent");
+}
 }
 
 }
